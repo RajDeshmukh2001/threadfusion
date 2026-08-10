e5d63d722c62dd1245770806427f236c9503d3ad
Prithviraj Deshmukh <97602045+RajDeshmukh2001@users.noreply.github.com>
2026-08-10 22:08:03 +0530

    Clean up project configuration

--- capstone/settings.py
+++ capstone/settings.py
@@ -13,22 +13,26 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d!^$v@au1a71&no2=l7ydy)^3l=lc^(nm^cp%7bvj9z#02n+=t'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in os.environ.get("ALLOWED_HOSTS", "").split(",")
+    if host.strip()
+]
 
 # Application definition
 
@@ -81,19 +85,10 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse('postgresql://threadfusion_17kn_user:u00TVBGe3HPkKH1d8U29CRue6HPB3i97@dpg-cqa061iju9rs73bbrud0-a.singapore-postgres.render.com/threadfusion_17kn')
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL")
+    )
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'threadfusion',
-#         'USER': 'postgres',
-#         'PASSWORD': 'rajdd2001',
-#         'PORT': '5432',
-#         'HOST': 'localhost',
-#     }
-# }
 
 AUTH_USER_MODEL = 'threadfusion.User'
 
