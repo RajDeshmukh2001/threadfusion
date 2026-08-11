68fbb2d27326726900d72641fd8309f313802e6f
Prithviraj Deshmukh <97602045+RajDeshmukh2001@users.noreply.github.com>
2026-08-11 15:00:31 +0530

    make Gunicorn port configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 python manage.py collectstatic --noinput
 
 # Start Gunicorn web server
-exec gunicorn --bind 0.0.0.0:8000 capstone.wsgi:application
+exec gunicorn --bind 0.0.0.0:${PORT:-8000} capstone.wsgi:application
